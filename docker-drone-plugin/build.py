3a350d978e21fc16ab7734b422ba3f814388efff
AndrewNeo <andrew@andrewneo.com>
2018-12-29 21:29:01 -0800

    Add more arguments to command-line version

--- docker-drone-plugin/build.py
+++ docker-drone-plugin/build.py
@@ -17,4 +17,6 @@ ignore_paths = None
 if 'PLUGIN_IGNORE' in os.environ:
     ignore_paths = os.environ['PLUGIN_IGNORE'].split(',')
 
+print("Processing.")
 sssg.process_directory(input_dir, target_dir, files_as_dirs=files_as_dirs, ignore_paths=ignore_paths)
+print("Done.")
--- sssg/__main__.py
+++ sssg/__main__.py
@@ -7,11 +7,17 @@ def main():
     parser.add_argument("source", help="Source directory")
     parser.add_argument("destination", help="Destination directory")
     parser.add_argument("-d", "--delete", action="store_true", help="Delete contents of destination directory before copying")
+    parser.add_argument("--files-as-dirs", action="store_true", help="Turn templated files into directories (Jekyll style)")
+    parser.add_argument("-i", "--ignore", action="store_const", help="Ignore glob (gitignore style, comma seperated)")
 
     args = parser.parse_args()
 
-    print("Running.")
-    process_directory(args.source, args.destination, args.delete)
+    ignore_paths = []
+    if args.ignore:
+        ignore_paths = args.ignore.split(",")
+
+    print("Processing.")
+    process_directory(args.source, args.destination, files_as_dirs=args.files_as_dirs, wipe_first=args.delete, ignore_paths=ignore_paths)
     print("Done.")
 
 
