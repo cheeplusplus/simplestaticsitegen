5b682c297ee0843efd99d7f823925a74e2e655d0
Jess Koester <jess@neocodenetworks.com>
2022-04-25 21:16:54 -0700

    Add copy support

--- sssg/__init__.py
+++ sssg/__init__.py
@@ -7,6 +7,7 @@ import json
 from jinja2 import Environment, FileSystemLoader, Markup
 from markdown import Markdown
 import frontmatter
+import urllib.request
 
 
 class BuildError(Exception):
@@ -117,6 +118,31 @@ def process_md_template(tmpl, source_filename, dest_filename, **kwargs):
         f.write(output)
 
 
+def process_copy_operation(source_filename, dest_filename):
+    '''Parse a SSSG-COPY file and save the target to the destination.'''
+
+    with open(source_filename, "r", encoding="utf-8") as f:
+        target_url = f.read()
+
+    if not target_url:
+        return
+
+    (scheme, _, _, _, _, _) = urllib.parse.urlparse(target_url)
+
+    if not scheme:
+        # Probably a local path, relative or otherwise
+        abs_target = Path(target_url)
+        if (not abs_target.is_absolute()):
+            abs_target = (Path(source_filename).parent / target_url).resolve()
+        if abs_target.exists():
+            shutil.copy2(abs_target, dest_filename)
+        else:
+            raise BuildError(f"Unable to find source file {target_url}")
+    else:
+        # Save the URL to a file
+        urllib.request.urlretrieve(target_url, dest_filename)
+
+
 def restructure_file_as_dir(files_as_dirs, current_path, current_height=0, new_filename="index.html"):
     '''Restructure "file.md" as "file/index.html", imitating Jekyll'''
     basedir = current_path.parent
@@ -200,9 +226,10 @@ def process_directory(source_dir, dest_dir, files_as_dirs=False, wipe_first=Fals
         if not os.path.exists(dest_dir_path):
             os.makedirs(dest_dir_path)
 
-        # Run anything ending in .j2 as a template
-        # Assume .md files are templates so they get turned into HTML
         if src_path.endswith(".j2") or src_path.endswith(".md"):
+            # Run anything ending in .j2 as a template
+            # Assume .md files are templates so they get turned into HTML
+
             dest_path_pure = dest_path.with_suffix("")
             ptr_height = len(pp)
 
@@ -223,6 +250,17 @@ def process_directory(source_dir, dest_dir, files_as_dirs=False, wipe_first=Fals
                     process_template(templater, src_path, dest_path_pure, path_to_root=ptr)
                 except Exception as err:
                     raise BuildError(f"Failed processing template file: /{'/'.join(pp)}, got error: {err}") from err
+        elif src_path.endswith(".sssg-copy"):
+            # Copy softlinks
+
+            dest_path_pure = dest_path.with_suffix("")
+
+            try:
+                process_copy_operation(src_path, dest_path_pure)
+            except Exception as err:
+                raise BuildError(f"Failed file copy: /{'/'.join(pp)}, got error: {err}") from err
+
         else:
             # Copy everything else
+
             shutil.copy2(src_path, dest_path)
